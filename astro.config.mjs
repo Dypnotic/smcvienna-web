23ecdcaff8a19cd5592442ad617c56f9f748d391
Dypnotic <jenscherukad@gmail.com>
2024-10-09 22:03:22 +0200

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
 
+import svelte from '@astrojs/svelte'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [svelte(), mdx()],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: 'modern',
+				}
+			}
+		},
+	}
 })
